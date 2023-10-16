5ef1c01317b818910a3b9273d97d53a0d53a0ad7
krixtomaram <146417609+krixtomaram@users.noreply.github.com>
2023-10-16 10:51:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ global.owner = [
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['919365354223','919365354223'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.prems = ['917605902011', '32460248586', '919365354223']
+global.allowed = ['917605902011', '32460248586', '919365354223']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,7 +52,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = '꧁༺𝚇𝙸𝙽𝚇𝙾༻꧂'
 global.premium = 'true'
-global.packname = '𝙺𝚁𝙸𝚇𝚄┃ᴮᴼᵀ' 
+global.packname = '꧁༺𝚇𝙸𝙽𝚇𝙾༻꧂' 
 global.author = '@𝙺𝚁𝙸𝚇𝚃𝙾𝙼' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
